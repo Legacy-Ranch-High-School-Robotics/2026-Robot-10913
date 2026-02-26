e73806f97d85363b581a855ef9d4b7c5e033b43f
ThePranav <kotasiddharthreddy@gmail.com>
2026-02-26 17:00:29 -0600

    Shooter AOK

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-Robot-10913";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 59;
+  public static final String GIT_SHA = "734dd568a008b238bc25387db50d463be2fbb3e7";
+  public static final String GIT_DATE = "2026-02-05 17:20:37 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-06 16:43:15 CST";
+  public static final long BUILD_UNIX_TIME = 1770417795264L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
