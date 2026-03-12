27c6df3becafd781489602b46fdfa6a812e85be5
jnthneLo <jonathan.e.lockett@gmail.com>
2026-03-12 17:11:18 -0500

    gafasasfaa

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,208 +1,110 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+package frc.robot.subsystems.intake;
 
-package frc.robot;
+import static frc.robot.subsystems.intake.IntakeConstants.*;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.OIConstants;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.shooter.ShooterConstants;
-import frc.robot.subsystems.hopper.Hopper;
-import frc.robot.subsystems.hopper.HopperConstants;
-import frc.robot.subsystems.intake.Intake;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import java.util.List;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/*
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
- */
-public class RobotContainer {
-  // The robot's subsystems
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final Shooter m_shooter = new Shooter();
-  private final Hopper m_hopper = new Hopper();
-  private final Intake m_intake = new Intake();
+public class Intake extends SubsystemBase {
+  private final SparkMax motor;
+  private final SparkMax liftMotor;
+  private final RelativeEncoder encoder;
+  //lift motor and encoder
+  private final RelativeEncoder liftEncoder;
 
-  // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
-  // The operator's controller
-  XboxController m_operatorController = new XboxController(OIConstants.kOperatorControllerPort);
+  public Intake() {
+    motor = new SparkMax(intakeMotorCanId, MotorType.kBrushless);
+    liftMotor = new SparkMax(intakeLiftMotorCanId, MotorType.kBrushless);
+    encoder = motor.getEncoder();
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();  
+    liftEncoder = liftMotor.getEncoder();
 
-    // Configure default commands
-    m_robotDrive.setDefaultCommand(
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
-            () -> m_robotDrive.drive(
-                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
-                true),
-            m_robotDrive));
+    var config = new SparkMaxConfig();
+    config
+        .inverted(intakeMotorInverted)
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(intakeCurrentLimit)
+        .voltageCompensation(12.0);
+    config.encoder.velocityConversionFactor(1.0);
+
+    var liftConfig = new SparkMaxConfig();
+    liftConfig
+        .inverted(intakeLiftMotorInverted)
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(intakeCurrentLimit)
+        .voltageCompensation(12.0);
+
+    motor.configure(
+        config,
+        com.revrobotics.ResetMode.kResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
+
+    liftMotor.configure(
+        config,
+        com.revrobotics.ResetMode.kResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
-   * subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
-   * passing it to a
-   * {@link JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // ========== DRIVER CONTROLS ==========
-    // Drive controls
-    new JoystickButton(m_driverController, Button.kR1.value)
-        .whileTrue(new RunCommand(
-            () -> m_robotDrive.setX(),
-            m_robotDrive));
+  @Override
+  public void periodic() {}
 
-    new JoystickButton(m_driverController, XboxController.Button.kStart.value)
-        .onTrue(new InstantCommand(
-            () -> m_robotDrive.zeroHeading(),
-            m_robotDrive));
-
-    // ========== OPERATOR CONTROLS ==========
-    // Eject button (A) - runs intake, hopper, and shooter backwards
-    new JoystickButton(m_operatorController, XboxController.Button.kB.value)
-        .whileTrue(new RunCommand(
-            () -> {
-              m_intake.outtake();
-              m_hopper.eject();
-              m_shooter.eject();
-            },
-            m_intake, m_hopper, m_shooter))
-        .onFalse(new InstantCommand(
-            () -> {
-              m_intake.stop();
-              m_hopper.stop();
-              m_shooter.stop();
-            },
-            m_intake, m_hopper, m_shooter));
-
-    new JoystickButton(m_operatorController, XboxController.Button.kA.value)
-        .whileTrue(new RunCommand(
-            () -> m_shooter.setVelocity(ShooterConstants.shooterRPM),
-            m_shooter))
-        .onFalse(new InstantCommand(
-            () -> m_shooter.stop(),
-            m_shooter));
-        .whileTrue(new RunCommand(
-            () -> {
-              m_intake.feed();
-            }));
-
-    // Intake controls
-    new JoystickButton(m_operatorController, XboxController.Button.kX.value)
-        .whileTrue(new RunCommand(
-            () -> m_intake.outtake(),
-            m_intake))
-        .onFalse(new InstantCommand(
-            () -> m_intake.stop(),
-            m_intake));
-
-    new JoystickButton(m_operatorController, XboxController.Button.kY.value)
-        .whileTrue(new RunCommand(
-            () -> m_intake.outtake(),
-            m_intake))
-        .onFalse(new InstantCommand(
-            () -> m_intake.stop(),
-            m_intake));
-
-new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value)
-        .whileTrue(new RunCommand(
-            () -> m_intake.liftDeploy(),
-            m_intake).until(m_intake::isLiftDeployed))
-        .onFalse(new InstantCommand(
-            () -> m_intake.liftStop(),
-            m_intake));
-
-    // Feed button (right bumper) - waits for shooter to reach target RPM before feeding
-    new JoystickButton(m_operatorController, XboxController.Button.kRightBumper.value)
-        .whileTrue(
-            // start spinning the shooter
-            new RunCommand(
-                () -> m_shooter.setTopVelocity(ShooterConstants.shooterRPM), m_shooter)
-                .until(m_shooter::atTargetVelocity)
-                // once at speed, start the conveyer
-                .andThen(new RunCommand(() -> m_intake.feed(), m_intake)))
-        .onFalse(new InstantCommand(
-            () -> m_intake.stop(),
-            m_intake));
+  public void intake() {
+    motor.setVoltage(intakeVoltage);
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // Create config for trajectory
-    TrajectoryConfig config = new TrajectoryConfig(
-        AutoConstants.kMaxSpeedMetersPerSecond,
-        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
-        .setKinematics(DriveConstants.kDriveKinematics);
+  public void intakeFeed(){
+    motor.setVoltage(liftVoltage);
+  }
 
-    // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
-        config);
+  public void liftRetract(){
+    if (getLiftPosition() > retractedPosition - liftPositionTolerance) {
+      liftMotor.setVoltage(liftVoltage * -1);
+    } else {
+      liftStop();
+    }
+  }
 
-    var thetaController = new ProfiledPIDController(
-        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+  public void liftDeploy(){
+    if (getLiftPosition() < deployedPosition + liftPositionTolerance) {
+      liftMotor.setVoltage(liftVoltage);
+    } else {
+      liftStop();
+    }
+  }
 
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-        exampleTrajectory,
-        m_robotDrive::getPose, // Functional interface to feed supplier
-        DriveConstants.kDriveKinematics,
+  public void outtake() {
+    motor.setVoltage(outtakeVoltage);
+  }
 
-        // Position controllers
-        new PIDController(AutoConstants.kPXController, 0, 0),
-        new PIDController(AutoConstants.kPYController, 0, 0),
-        thetaController,
-        m_robotDrive::setModuleStates,
-        m_robotDrive);
+  public void feed() {
+    motor.setVoltage(feedVoltage);
+  }
 
-    // Reset odometry to the starting pose of the trajectory.
-    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+  public void stop() {
+    motor.stopMotor();
+  }
 
-    // Run path following command, then stop at the end.
-    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
+  public void liftStop() {
+    liftMotor.stopMotor();
+  }
+
+  public double getVelocityRPM() {
+    return encoder.getVelocity();
+  }
+
+  public double getLiftPosition() {
+    return liftEncoder.getPosition();
+  }
+
+  public boolean isLiftDeployed() {
+    return Math.abs(getLiftPosition() - deployedPosition) < liftPositionTolerance;
+  }
+
+  public boolean isLiftRetracted() {
+    return Math.abs(getLiftPosition() - retractedPosition) < liftPositionTolerance;
   }
 }
