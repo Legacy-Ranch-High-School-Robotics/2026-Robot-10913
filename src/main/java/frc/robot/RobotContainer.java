4e28bd2dfd10f007c8e00fdc9b0922bfed671682
jnthneLo <jonathan.e.lockett@gmail.com>
2026-03-13 12:36:15 -0500

    upload

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,61 +5,54 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
-import frc.robot.Constants.AutoConstants;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterConstants;
 import frc.robot.subsystems.hopper.Hopper;
-import frc.robot.subsystems.hopper.HopperConstants;
 import frc.robot.subsystems.intake.Intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import java.util.List;
+
+import com.pathplanner.lib.auto.AutoBuilder;
 
 /*
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems
+  // Subsystems
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
   private final Shooter m_shooter = new Shooter();
   private final Hopper m_hopper = new Hopper();
   private final Intake m_intake = new Intake();
 
-  // The driver's controller
+  // Autonomous Chooser
+  private final SendableChooser<Command> autoChooser;
+
+  // Controllers
   XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
-  // The operator's controller
   XboxController m_operatorController = new XboxController(OIConstants.kOperatorControllerPort);
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();  
 
-    // Configure default commands
+    // Build PathPlanner Auto Chooser
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Mode", autoChooser);
+
+    // Default drive command
     m_robotDrive.setDefaultCommand(
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
         new RunCommand(
             () -> m_robotDrive.drive(
                 -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
@@ -69,101 +62,57 @@ public class RobotContainer {
             m_robotDrive));
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
-   * subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
-   * passing it to a
-   * {@link JoystickButton}.
-   */
   private void configureButtonBindings() {
     // ========== DRIVER CONTROLS ==========
-    // Drive controls
-    new JoystickButton(m_driverController, Button.kR1.value)
-        .whileTrue(new RunCommand(
-            () -> m_robotDrive.setX(),
-            m_robotDrive));
+    // X-Stance
+    new JoystickButton(m_driverController, XboxController.Button.kRightBumper.value)
+        .whileTrue(new RunCommand(m_robotDrive::setX, m_robotDrive));
 
+    // Reset Gyro
     new JoystickButton(m_driverController, XboxController.Button.kStart.value)
-        .onTrue(new InstantCommand(
-            () -> m_robotDrive.zeroHeading(),
-            m_robotDrive));
+        .onTrue(new InstantCommand(m_robotDrive::zeroHeading, m_robotDrive));
 
     // ========== OPERATOR CONTROLS ==========
-    // Eject button (B) - runs intake, hopper, and shooter backwards
+    
+    // Combined A Button: Runs both Shooter and Hopper
+    new JoystickButton(m_operatorController, XboxController.Button.kA.value)
+        .whileTrue(new RunCommand(() -> {
+            m_shooter.setVelocity(ShooterConstants.shooterRPM);
+            m_hopper.setVelocity(ShooterConstants.shooterRPM);
+        }, m_shooter, m_hopper))
+        .onFalse(new InstantCommand(() -> {
+            m_shooter.stop();
+            m_hopper.stop();
+        }, m_shooter, m_hopper));
+
+    // Eject (B)
     new JoystickButton(m_operatorController, XboxController.Button.kB.value)
-        .whileTrue(new RunCommand(
-            () -> {
+        .whileTrue(new RunCommand(() -> {
               m_intake.outtake();
               m_hopper.eject();
               m_shooter.eject();
-            },
-            m_intake, m_hopper, m_shooter))
-        .onFalse(new InstantCommand(
-            () -> {
+            }, m_intake, m_hopper, m_shooter))
+        .onFalse(new InstantCommand(() -> {
               m_intake.stop();
               m_hopper.stop();
               m_shooter.stop();
-            },
-            m_intake, m_hopper, m_shooter));
+            }, m_intake, m_hopper, m_shooter));
 
-    new JoystickButton(m_operatorController, XboxController.Button.kA.value)
-        .whileTrue(new RunCommand(
-            () -> m_shooter.setVelocity(ShooterConstants.shooterRPM),
-            m_shooter))
-        .onFalse(new InstantCommand(
-            () -> m_shooter.stop(),
-            m_shooter));
-      new JoystickButton(m_operatorController, XboxController.Button.kA.value)
-        .whileTrue(new RunCommand(
-            () -> m_hopper.setVelocity(ShooterConstants.shooterRPM),
-            m_shooter))
-        .onFalse(new InstantCommand(
-            () -> m_hopper.stop(),
-            m_shooter));
-      
-    
-    // Intake controls
+    // Intake (X)
     new JoystickButton(m_operatorController, XboxController.Button.kX.value)
-        .whileTrue(new RunCommand(
-            () -> m_intake.intake(),
-            m_intake))
-        .onFalse(new InstantCommand(
-            () -> m_intake.stop(),
-            m_intake));
-        
+        .whileTrue(new RunCommand(m_intake::intake, m_intake))
+        .onFalse(new InstantCommand(m_intake::stop, m_intake));
 
+    // Outtake (Y)
     new JoystickButton(m_operatorController, XboxController.Button.kY.value)
-        .whileTrue(new RunCommand(
-            () -> m_intake.outtake(),
-            m_intake))
-        .onFalse(new InstantCommand(
-            () -> m_intake.stop(),
-            m_intake));
+        .whileTrue(new RunCommand(m_intake::outtake, m_intake))
+        .onFalse(new InstantCommand(m_intake::stop, m_intake));
 
-new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value)
-        .whileTrue(new RunCommand(
-            () -> m_intake.liftDeploy(),
-            m_intake).until(m_intake::isLiftDeployed))
-        .onFalse(new InstantCommand(
-            () -> m_intake.liftStop(),
-            m_intake));
-
-    // Feed button (right bumper) - waits for shooter to reach target RPM before feeding
-    new JoystickButton(m_operatorController, XboxController.Button.kRightBumper.value)
-        .whileTrue(
-            // start spinning the shooter
-            new RunCommand(
-                () -> m_shooter.setTopVelocity(ShooterConstants.shooterRPM), m_shooter)
-                .until(m_shooter::atTargetVelocity)
-                // once at speed, start the conveyer
-                .andThen(new RunCommand(() -> m_intake.feed(), m_intake)))
-        .onFalse(new InstantCommand(
-            () -> m_intake.stop(),
-            m_intake));
-  }
+    // Intake Lift (Left Bumper)
+    new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value)
+        .whileTrue(new RunCommand(m_intake::liftDeploy, m_intake).until(m_intake::isLiftDeployed))
+        .onFalse(new InstantCommand(m_intake::liftStop, m_intake));
+  };
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -171,43 +120,6 @@ new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // Create config for trajectory
-    TrajectoryConfig config = new TrajectoryConfig(
-        AutoConstants.kMaxSpeedMetersPerSecond,
-        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
-        .setKinematics(DriveConstants.kDriveKinematics);
-
-    // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
-        config);
-
-    var thetaController = new ProfiledPIDController(
-        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-        exampleTrajectory,
-        m_robotDrive::getPose, // Functional interface to feed supplier
-        DriveConstants.kDriveKinematics,
-
-        // Position controllers
-        new PIDController(AutoConstants.kPXController, 0, 0),
-        new PIDController(AutoConstants.kPYController, 0, 0),
-        thetaController,
-        m_robotDrive::setModuleStates,
-        m_robotDrive);
-
-    // Reset odometry to the starting pose of the trajectory.
-    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
-
-    // Run path following command, then stop at the end.
-    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
+    return autoChooser.getSelected();
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -14,9 +14,16 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import com.ctre.phoenix6.hardware.Pigeon2;
-import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
   // Create MAXSwerveModules
@@ -44,7 +51,7 @@ public class DriveSubsystem extends SubsystemBase {
   private final Pigeon2 m_gyro = new Pigeon2(DriveConstants.kPigeon2CanId);
 
   // Odometry class for tracking robot pose
-  SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
+  private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
       DriveConstants.kDriveKinematics,
       Rotation2d.fromDegrees(m_gyro.getYaw().getValueAsDouble()),
       new SwerveModulePosition[] {
@@ -58,6 +65,29 @@ public class DriveSubsystem extends SubsystemBase {
   public DriveSubsystem() {
     // Usage reporting for MAXSwerve template
     HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDriveSwerve_MaxSwerve);
+
+    // PathPlanner AutoBuilder Configuration - MUST be inside the constructor
+    try {
+      RobotConfig config = RobotConfig.fromGUISettings();
+      AutoBuilder.configure(
+          this::getPose, 
+          this::resetPose, 
+          this::getRobotRelativeSpeeds, 
+          (speeds, feedforwards) -> driveRobotRelative(speeds), 
+          new PPHolonomicDriveController(
+              new PIDConstants(5.0, 0.0, 0.0), // Translation PID
+              new PIDConstants(5.0, 0.0, 0.0)  // Rotation PID
+          ),
+          config,
+          () -> {
+              var alliance = DriverStation.getAlliance();
+              return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red;
+          },
+          this
+      );
+    } catch (Exception e) {
+      DriverStation.reportError("Failed to load PathPlanner config", e.getStackTrace());
+    }
   }
 
   @Override
@@ -73,20 +103,32 @@ public class DriveSubsystem extends SubsystemBase {
         });
   }
 
-  /**
-   * Returns the currently-estimated pose of the robot.
-   *
-   * @return The pose.
-   */
   public Pose2d getPose() {
     return m_odometry.getPoseMeters();
   }
 
-  /**
-   * Resets the odometry to the specified pose.
-   *
-   * @param pose The pose to which to set the odometry.
-   */
+  public void resetPose(Pose2d pose) {
+    resetOdometry(pose);
+  }
+
+  /** Returns the current robot-relative speeds. Used by PathPlanner. */
+  public ChassisSpeeds getRobotRelativeSpeeds() {
+    return DriveConstants.kDriveKinematics.toChassisSpeeds(
+        new SwerveModuleState[] {
+            m_frontLeft.getState(),
+            m_frontRight.getState(),
+            m_rearLeft.getState(),
+            m_rearRight.getState()
+        }
+    );
+  }
+
+  /** Drive the robot using robot-relative speeds. Used by PathPlanner. */
+  public void driveRobotRelative(ChassisSpeeds speeds) {
+    SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(speeds);
+    setModuleStates(moduleStates);
+  }
+
   public void resetOdometry(Pose2d pose) {
     m_odometry.resetPosition(
         Rotation2d.fromDegrees(m_gyro.getYaw().getValueAsDouble()),
@@ -99,17 +141,7 @@ public class DriveSubsystem extends SubsystemBase {
         pose);
   }
 
-  /**
-   * Method to drive the robot using joystick info.
-   *
-   * @param xSpeed        Speed of the robot in the x direction (forward).
-   * @param ySpeed        Speed of the robot in the y direction (sideways).
-   * @param rot           Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the
-   *                      field.
-   */
   public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-    // Convert the commanded speeds into the correct units for the drivetrain
     double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
     double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
     double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
@@ -119,17 +151,10 @@ public class DriveSubsystem extends SubsystemBase {
             ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
                 Rotation2d.fromDegrees(m_gyro.getYaw().getValueAsDouble()))
             : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
-    SwerveDriveKinematics.desaturateWheelSpeeds(
-        swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
-    m_frontLeft.setDesiredState(swerveModuleStates[0]);
-    m_frontRight.setDesiredState(swerveModuleStates[1]);
-    m_rearLeft.setDesiredState(swerveModuleStates[2]);
-    m_rearRight.setDesiredState(swerveModuleStates[3]);
+            
+    setModuleStates(swerveModuleStates);
   }
 
-  /**
-   * Sets the wheels into an X formation to prevent movement.
-   */
   public void setX() {
     m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
     m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
@@ -137,11 +162,6 @@ public class DriveSubsystem extends SubsystemBase {
     m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
   }
 
-  /**
-   * Sets the swerve ModuleStates.
-   *
-   * @param desiredStates The desired SwerveModule states.
-   */
   public void setModuleStates(SwerveModuleState[] desiredStates) {
     SwerveDriveKinematics.desaturateWheelSpeeds(
         desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
@@ -151,35 +171,11 @@ public class DriveSubsystem extends SubsystemBase {
     m_rearRight.setDesiredState(desiredStates[3]);
   }
 
-  /** Resets the drive encoders to currently read a position of 0. */
-  public void resetEncoders() {
-    m_frontLeft.resetEncoders();
-    m_rearLeft.resetEncoders();
-    m_frontRight.resetEncoders();
-    m_rearRight.resetEncoders();
-  }
-
-  /** Zeroes the heading of the robot. */
   public void zeroHeading() {
     m_gyro.setYaw(0);
   }
 
-  
-  /**
-   * Returns the heading of the robot.
-   *
-   * @return the robot's heading in degrees, from -180 to 180
-   */
   public double getHeading() {
     return Rotation2d.fromDegrees(m_gyro.getYaw().getValueAsDouble()).getDegrees();
-  }
-
-  /**
-   * Returns the turn rate of the robot.
-   *
-   * @return The turn rate of the robot, in degrees per second
-   */
-  public double getTurnRate() {
-    return m_gyro.getAngularVelocityZWorld().getValueAsDouble() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
   }
 }
